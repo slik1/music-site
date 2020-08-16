efd8d0d53dd69ec53819cdcd5c3d3fca29a7fc0b
Gary Saunders <gary@slikinteractive.com>
2020-08-15 23:17:50 -0700

    Messing around

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
+  // Material Style Basic Audio Player Title and Audio URL
+  msbapTitle = 'Limo Bus';
+  msbapAudioUrl = 'http://g-slik.com/BEATS/Limo_Bus_INSTRUMENTAL.mp3';   
+  msaapDisplayVolumeControls = true;
+
+  msbapDisplayTitle = false;   
+
   title = 'music-site';
 }
